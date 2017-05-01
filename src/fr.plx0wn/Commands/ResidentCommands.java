c28e7dfe64dc6c99079d8939595593db9400df5c
Plx0wn <lucas.rouss@gmail.com>
2017-05-01 11:12:27 +0200

    Update ResidentCommands.java

--- src/fr.plx0wn/Commands/ResidentCommands.java
+++ src/fr.plx0wn/Commands/ResidentCommands.java
@@ -1,67 +1,51 @@
 package fr.plx0wn.Commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import fr.plx0wn.MSTown;
+import fr.plx0wn.API.Resident;
 
 public class ResidentCommands implements CommandExecutor {
+	
+	public static String colored(String msg) {
+		return ChatColor.translateAlternateColorCodes('&', msg);
+	}
 
-	static FileConfiguration cityconfig = MSTown.cityConfig;
-	static FileConfiguration msgconfig = MSTown.msgConfig;
+	static FileConfiguration cityConfig = MSTown.cityConfig;
+	static FileConfiguration msgConfig = MSTown.msgConfig;
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (label.equalsIgnoreCase("resident")) {
 			if (args.length == 0) {
-				// LIST COMMANDS
+				sender.sendMessage(colored("&a---[ Commandes ]---"));
+				sender.sendMessage(colored("&a- /resident add [nom de la ville] [nom du joueur]"));
+				sender.sendMessage(colored("&a- /resident remove [nom de la ville] [nom du joueur]"));
 			}
 			
-//			// RESIDENT ADD
-//			
-//			if (sender.hasPermission("resident.add")) {
-//				if (args.length == 1) {
-//					if (args[0].equalsIgnoreCase("add")) {
-//						sender.sendMessage(API.cs(msgconfig.getString("commands.resident-add-error")));
-//					}
-//				}
-//				if (args.length == 2) {
-//					if (args[0].equalsIgnoreCase("add")) {
-//						sender.sendMessage(API.cs(msgconfig.getString("commands.resident-add-error")));
-//					}
-//				}
-//				if (args.length == 3) {
-//					if (args[0].equalsIgnoreCase("add")) {
-//						API.addPlayerToCity(sender, args[1], args[2]);
-//					}
-//				}
-//			} else {
-//				sender.sendMessage(API.cs(msgconfig.getString("commands.no-permissions")));
-//			}
-//			
-//			// RESIDENT REMOVE
-//			
-//			if (sender.hasPermission("resident.remove")) {
-//				if (args.length == 1) {
-//					if (args[0].equalsIgnoreCase("remove")) {
-//						sender.sendMessage(API.cs(msgconfig.getString("commands.resident-remove-error")));
-//					}
-//				}
-//				if (args.length == 2) {
-//					if (args[0].equalsIgnoreCase("remove")) {
-//						sender.sendMessage(API.cs(msgconfig.getString("commands.resident-remove-error")));
-//					}
-//				}
-//				if (args.length == 3) {
-//					if (args[0].equalsIgnoreCase("remove")) {
-//						API.removePlayerToCity(sender, args[1], args[2]);
-//					}
-//				}
-//			} else {
-//				sender.sendMessage(API.cs(msgconfig.getString("commands.no-permissions")));
-//			}
+			if(args.length == 1){
+				if(args[0].equalsIgnoreCase("add")){
+					sender.sendMessage(colored(msgConfig.getString("commands.resident-add-error")));
+				}
+			}
+			
+			if(args.length == 2){
+				if(args[0].equalsIgnoreCase("add")){
+					sender.sendMessage(colored(msgConfig.getString("commands.resident-add-error")));
+				}
+			}
+			if(args.length == 3){
+				if(args[0].equalsIgnoreCase("add")){
+					Resident.addResident(sender, args[1], args[2]);
+				}
+				if(args[0].equalsIgnoreCase("remove")){
+					Resident.removeResident(sender, args[1], args[2]);
+				}
+			}
 			
 		}
 		return false;
